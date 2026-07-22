71ede16440230159e40c3d9b24f258732ab70f6b
dinilpjose <diniljosework@gmail.com>
2026-07-22 21:32:39 +0300

    disabled cors

--- src/common/secuirities/secuirity.ts
+++ src/common/secuirities/secuirity.ts
@@ -25,21 +25,26 @@ const allowedHeaders = [
   'Content-Type',
 ];
 
-// export async function setupCors(app: NestFastifyApplication) {
-//   await app.register(fastifyCors, {
-//     origin: (origin, callback) => {
-//       if (origin === undefined || whitelist.has(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'), false);
-//       }
-//     },
-//     allowedHeaders: allowedHeaders,
-//     methods: ['GET', 'PATCH', 'POST', 'DELETE'],
-//     exposedHeaders: 'cookies',
-//     credentials: true,
-//   });
-// }
+export async function setupCors(app: NestFastifyApplication) {
+
+await app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+});
+  // await app.register(fastifyCors, {
+  //   origin: (origin, callback) => {
+  //     if (origin === undefined || whitelist.has(origin)) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'), false);
+  //     }
+  //   },
+  //   allowedHeaders: allowedHeaders,
+  //   methods: ['GET', 'PATCH', 'POST', 'DELETE'],
+  //   exposedHeaders: 'cookies',
+  //   credentials: true,
+  // });
+}
 
 
 export async function setupHelmets(app: NestFastifyApplication) {
